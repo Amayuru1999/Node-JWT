18ebd7dbb6aa1cc8d28a6c06d73510b6e8432729
Amayuru1999 <amarasingheau@gmail.com>
2024-05-03 09:44:13 +0530

    first commit

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
+const authRoutes = require('./routes/authRoutes');
 
 require('dotenv').config();
 // middleware
 app.use(express.static('public'));
+app.use(express.json())
 
 // view engine
 app.set('view engine', 'ejs');
 const mongoURL = process.env.MONGODB_URL;
 // database connection
 mongoose.connect(mongoURL, {
-  useNewUrlParser: true, // no longer needed
-  useUnifiedTopology: true // no longer needed
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 .then(() => {
   console.log('Connected to MongoDB');
-  // Your code here
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 })
 .catch(err => {
   console.error('Error connecting to MongoDB:', err.message);
@@ -25,3 +29,4 @@ mongoose.connect(mongoURL, {
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
